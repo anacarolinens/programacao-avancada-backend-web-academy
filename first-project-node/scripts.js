506746794a24f621977b159eca40e1ce7c1c0f25
Ana Caroline <caroline.ana.n.s@gmail.com>
2023-06-26 19:25:08 -0500

    Parte 1 aula 26/06/2023

--- first-project-node/scripts.js
+++ first-project-node/scripts.js
@@ -2,10 +2,82 @@
 // setTimeout(function myFunction(){ //vira uma promisse
 //     console.log('Esperando 5 segundo para aparecer')
 // }, 5000)
-
+/*
 console.log('Quando isso aparece?')
 
 //Arrow function
-setTimeout(() => {
-    console.log('Esperando 5 segundo para aparecer')
-}, 5000);
+setTimeout(() => { //lista de argumentos
+    console.log('Esperando 5 segundo para aparecer') 
+}, 5000);*/
+
+/*
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`); 
+});*/
+
+/*const fs = require('fs')
+console.log('Primeira saída')
+fs.readFile('./doc.txt', callback)
+
+//tentar transformar em uma função anônima
+function callback(err, conteudo) {
+    if(err)
+        return console.error(err)
+        console.log(String(conteudo))
+    
+}
+console.log('Segunda saída')
+console.log('Terceira saída')*/
+
+
+//calback - é uma função sendo passada/chamada dentro de uma lista de argumentos
+//lista de argumentos - isntruções
+
+
+// function soma(){
+//     const resultado = 5 + 56
+//     if (resultado === 61)
+//         sucesso()
+//     else falha()
+
+// }
+
+// function sucesso(){
+//     console.log('Sucesso! a soma é 61.')
+
+
+// }
+
+// function falha(){
+//     console.log('Falha! a soma é 61.')
+
+
+// }
+
+// soma()
+
+
+function soma(callbackSucesso, callbackErro){
+    const resultado = 5 + 56
+    if (resultado === 61)
+        callbackSucesso()
+    else callbackErro()
+
+}
+
+
+soma(
+    () =>  {console.log('Sucesso! a soma é 61.')}, 
+    () => {console.log('Falha! a soma é 61.')
+})
