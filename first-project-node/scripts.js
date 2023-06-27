8063bf31198f21c31da1b47b131aafc20ee9385d
Ana Caroline <caroline.ana.n.s@gmail.com>
2023-06-26 20:22:17 -0500

    Fim aula 26/06/2023

--- first-project-node/scripts.js
+++ first-project-node/scripts.js
@@ -68,16 +68,97 @@ console.log('Terceira saída')*/
 // soma()
 
 
-function soma(callbackSucesso, callbackErro){
-    const resultado = 5 + 56
-    if (resultado === 61)
-        callbackSucesso()
-    else callbackErro()
+// function soma(callbackSucesso, callbackErro){
+//     const resultado = 5 + 56
+//     if (resultado === 61)
+//         callbackSucesso()
+//     else callbackErro()
 
-}
+// }
 
 
-soma(
-    () =>  {console.log('Sucesso! a soma é 61.')}, 
-    () => {console.log('Falha! a soma é 61.')
+// soma(
+//     () =>  {console.log('Sucesso! a soma é 61.')}, 
+//     () => {console.log('Falha! a soma é 61.')
+// })
+
+
+//Promisses - (como se fosse literalmente uma promessa para dar certo ou errado) classes que possuiem métodos específicos
+//resultado de uma execução for concluido com sucesso ou quando fugir do esperado 
+
+//no construtir, é passada como argumento uma função anônima que possui dois parâmetros:
+//resolve - o que vai ser executado quando o resultado não for o esperado;
+//reject - o que vai ser executada quando o resultado não for o esperado;
+
+//resolve e reject são metodos que, quando executados, também passam argumenyos que são resolvidos no then e no catch
+
+//promisse serve para tirar de escopo de funções e trazerpara um escopo de um construtor
+//callback dentro de callback
+//onde eu escrevo é o parâmetro - é o que eu preciso
+//argumento é a resposta
+// const p = new Promise( (resolve, reject) => {
+//     const resultado = 5 + 56
+//     if (resultado === 62)
+//         resolve('Sucesso! a soma é 61.')
+//     else reject('Falha! a soma é 61.')
+// })
+
+// p
+// .then((mensagem) => {
+//     console.log(mensagem)
+// })
+// .catch((mensagem) => {
+//     console.log(mensagem)
+// })
+
+
+//voltando para callback
+
+// const melhorProgamador = 'caroline'
+
+// function oMelhor(calback, callbackErro) {
+//     if(melhorProgamador == 'Ana') {
+//         calback({
+//             nome: melhorProgamador,
+//             mensagem: 'A melhor!'
+//         })
+
+//     }else {
+//         callbackErro({
+//             mensagem01: 'Errado!',
+//             mensagem02: melhorProgamador + ' sério? '
+//         })
+//     }
+// }
+
+// oMelhor((objeto) => {
+//     console.log(objeto.nome + ' é ' + objeto.mensagem)
+
+// },
+//         (objeto) => {
+//     console.log(objeto.mensagem02 + objeto.mensagem01)
+// })
+
+
+//Desafio: Transformar o código acima para promise
+const a = new Promise ((resolve, reject) => {
+    const melhorProgamador = 'Ana'
+    if(melhorProgamador === 'Ana') {
+        resolve({
+            nome: melhorProgamador,
+            mensagem: 'A melhor!'
+        })
+
+    }else reject ({
+            mensagem01: 'Errado!',
+            mensagem02: melhorProgamador + ' sério? '
+        })
+})
+
+a.then ((objeto) => {
+    console.log(objeto.nome + ' é ' + objeto.mensagem)
+
+})
+.catch((objeto) => {
+    console.log(objeto.mensagem02 + objeto.mensagem01)
 })
